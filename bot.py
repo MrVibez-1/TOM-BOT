d11c8ab0bc5eddb1ee4935d347aaf6f577078cad
MrVibez <64214403+MrVibez-1@users.noreply.github.com>
2022-07-19 08:20:56 +0100

    a

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
-from http import client
+
 import json
 import sys
-from instabot import Bot
 import nextcord #pip install nextcord
 import os
 import random
@@ -22,10 +21,12 @@ Intents = nextcord.Intents.default()
 #When the Bot is ready it will print a ready message
 @client.event
 async def on_ready():
+    await client.change_presence(activity=nextcord.Game(name="!help"))
     print("\n / _ \ | |   |_   _| |_   _|  _  |  \/  | / __  \|  _  ||___  /\n/ /_\ \| |     | |     | | | | | | .  . | `' / /'| |_| |   / /\n|  _  || |     | |     | | | | | | |\/| |   / /  \____ |  / /  \n| | | || |_____| |_    | | \ \_/ / |  | | ./ /___.___/ /./ /  \n\_| |_/\_____/\___/    \_/  \___/\_|  |_/ \_____/\____/ \_/    \n                   By Alitom297#7175n")
     time.sleep(2)
     print('Bot is ready!')
     print(f'We have logged in as {client.user}')
+    
 
 
 #COGS
@@ -57,13 +58,38 @@ async def reload(ctx, extension):
 #Commands    
 @client.command()
 async def ping(ctx):
-    await ctx.send("Pong!")
+    await ctx.send("Pong! {}ms".format(round(client.latency * 1000)))
     
 @client.command()
-async def headortails(ctx, answer):
+async def head(ctx, answer):
+    await ctx.send(f"Bot Making Move")
+    time.sleep(2)
     if random.choice(["heads", "tails"]) == answer:
-        await ctx.reply("Congratulations")
+        await ctx.reply("Congratulations You Win")
     else:
         await ctx.reply("Sorry you lost")
+        
+@client.event
+async def on_member_join(member):
+    print(f"{member} has joined the server.")
+    channel = client.get_channel(965222587435216938)
+    await channel.send(f"{member} has joined the server.")
 
+@client.event
+async def on_member_left(member):
+    print(f"{member} has left the server.")
+    channel = client.get_channel(965222587435216938)
+    await channel.send(f"{member} has left the server.")
+    
+@client.command()
+async def unban(ctx, *, member):
+    banned_users = await ctx.guild.bans()
+    member_name, member_discriminator = member.split('#')
+    
+    for ban_Entry in banned_users:
+        user = ban_Entry.user
+        
+        if (user.name, user.discriminator) == (member_name, member_discriminator):
+            await ctx.guild.unban(user)
+    
 client.run(TOKEN)
--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,13 +1,23 @@
 from nextcord.ext import commands
 import nextcord
 
-class Basic(commands.Cog):
+class Ban(commands.Cog):
     def __init__(self, client):
         super().__init__()
-    
-    @commands.command()
-    async def hello(self, ctx):
-        await ctx.send(f'Hello {ctx.author.mention}!')
+        
+    @commands.command() 
+    async def ban(ctx, member: nextcord.Member, *, reason=None):
+       await member.ban(reason=reason)
+       await ctx.send(f"Banned {member.mention}")
+
 
 def setup(client):
-    client.add_cog(Basic(client))
+    client.add_cog(Kick(client))
+class Kick(commands.Cog):
+    def __init__(self, client):
+        super().__init__()
+        
+    @commands.command() 
+    async def kick(ctx, member: nextcord.Member, *, reason=None):
+       await member.kick(reason=reason)
+       await ctx.send(f"Banned {member.mention}")
